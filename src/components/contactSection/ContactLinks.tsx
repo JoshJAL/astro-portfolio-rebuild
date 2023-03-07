406429f715f29d9d6652da1405b764dd18137494
Joshua Levine <joshuaalevine3@gmail.com>
2023-03-07 00:36:59 -0500

    completed contact info section with link components

---
+++ src/components/contactSection/ContactLinks.tsx
@@ -0,0 +1,18 @@
+import { IoDocumentTextOutline, IoLogoFacebook, IoLogoGithub, IoLogoGoogle, IoLogoInstagram, IoLogoLinkedin } from "react-icons/io5";
+import { ImKeyboard } from "react-icons/im";
+import FancyLink from "./FancyLink";
+import { EMAIL, RESUME, THINGS_I_USE } from "../../consts";
+
+export default function ContactLinks() {
+  return (
+    <div className="w-full flex flex-col items-start">
+      <FancyLink Icon={IoLogoLinkedin} text='Joshua Levine' href="https://www.linkedin.com/in/josh-lev/" target="_blank" />
+      <FancyLink Icon={IoLogoGoogle} text='Gmail' href={"mailto:" + EMAIL} target="_blank" />
+      <FancyLink Icon={IoLogoGithub} text='@JoshJAL' href="https://github.com/JoshJAL" target="_blank" />
+      <FancyLink Icon={IoLogoInstagram} text='@JoshuaALevine3' href="https://www.instagram.com/joshuaalevine3/" target="_blank" />
+      <FancyLink Icon={IoLogoFacebook} text='Facebook' href="https://www.facebook.com/joshua.levine.520" target="_blank" />
+      <FancyLink Icon={IoDocumentTextOutline} text='Resume' href={RESUME} target="_blank" />
+      <FancyLink Icon={ImKeyboard} text='Things I Use' href={THINGS_I_USE} target="_blank" />
+    </div>
+  )
+}
---
+++ src/components/contactSection/FancyLink.tsx
@@ -0,0 +1,21 @@
+import type { IconType } from "react-icons/lib";
+
+interface FancyLinkProps {
+  href: string;
+  text: string;
+  Icon: IconType
+  target?: '_blank' | '_self' | '_parent' | '_top';
+}
+
+export default function FancyLink({ href, text, Icon, target }: FancyLinkProps) {
+  return (
+    <div className="flex flex-col">
+      <a target={target ? target : '_self'} href={href} className="flex basis-0 items-center gap-3 font-semibold text-lg text-orange-600 dark:text-teal-300 px-4 py-3 hover:bg-orange-400 hover:bg-opacity-20 rounded-lg transition-all ease-in-out duration-200 hover:underline underline-offset-2 dark:hover:bg-teal-700 dark:hover:bg-opacity-20">
+        <Icon />
+        <p>
+          {text}
+        </p>
+      </a>
+    </div>
+  )
+}
