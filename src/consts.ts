1f4c214455227158a5c6534c44dbe86e99f39f4c
JoshJAL <JoshuaALevine3@gmail.com>
2023-03-06 20:53:42 -0500

    astro rebuild with theme set up

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+export const CONTACT_ME = '/contactMe';
+export const EMAIL = 'JoshuaALevine3@gmail.com';
+export const PROJECTS = '/projects';
+// Might need to change this to a different link
+export const SOURCE = 'https://github.com/JoshJAL/updated-portfolio';
+export const RESUME = 'https://docs.google.com/document/d/1254COJOSGdsHpjNcSXf-VtdDhUxNAaB2nkCgap_aUQk/';
+export const THINGS_I_USE = 'https://things-i-use.vercel.app/';
+export const SITE_TITLE = 'Joshua Levine';
+export const SITE_DESCRIPTION = 'Joshua Levine\'s Portfolio';
