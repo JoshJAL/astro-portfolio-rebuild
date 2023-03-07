f5f0f303ccfaf758256001cadf334860c766c653
Joshua Levine <joshuaalevine3@gmail.com>
2023-03-07 02:02:25 -0500

    create progressive img component

---
+++ src/components/progressiveImg/ProgressiveImg.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+interface ProgressiveImgProps {
+  placeholder: string;
+  src: string;
+  alt: string;
+  className?: string;
+}
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+function ProgressiveImg({ placeholder, src, alt, className }: ProgressiveImgProps) {
+  const [imgSrc, setImgSrc] = useState(placeholder || src);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => setImgSrc(src);
+  }, [src]);
+
+  return (
+    <img
+      src={imgSrc}
+      alt={alt}
+      className={classNames(className ? className : '', imgSrc === placeholder ? 'filter blur-sm' : 'filter blur-none transition-all ease-out duration-200')}
+    />
+  )
+}
+
+export default ProgressiveImg
