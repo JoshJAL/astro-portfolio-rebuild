db70a4a73fbbe7bfab90c75f3a1042258e8d4011
JoshJAL <JoshuaALevine3@gmail.com>
2023-03-08 10:13:42 -0500

    remove script add page transition to css

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ export const CONTACT_ME = '/contactMe';
 export const EMAIL = 'JoshuaALevine3@gmail.com';
 export const PROJECTS = '/projects';
 // Might need to change this to a different link
-export const SOURCE = 'https://github.com/JoshJAL/updated-portfolio';
+export const SOURCE = 'https://github.com/JoshJAL/astro-portfolio-rebuild';
 export const RESUME =
   'https://docs.google.com/document/d/1254COJOSGdsHpjNcSXf-VtdDhUxNAaB2nkCgap_aUQk/edit?usp=sharing';
 export const THINGS_I_USE = 'https://things-i-use.vercel.app/';
