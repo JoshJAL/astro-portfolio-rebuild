e5ee43602f488c574e0a710caad02890442ee8a0
Joshua Levine <joshuaalevine3@gmail.com>
2023-03-08 23:53:25 -0500

    update site description

--- src/consts.ts
+++ src/consts.ts
@@ -7,4 +7,5 @@ export const RESUME =
   'https://docs.google.com/document/d/1254COJOSGdsHpjNcSXf-VtdDhUxNAaB2nkCgap_aUQk/edit?usp=sharing';
 export const THINGS_I_USE = 'https://things-i-use.vercel.app/';
 export const SITE_TITLE = 'Joshua Levine';
-export const SITE_DESCRIPTION = "Joshua Levine's Portfolio";
+export const SITE_DESCRIPTION =
+  "My Portfolio! Please take some time to learn a little bit about me and don't hesitate to reach out!";
