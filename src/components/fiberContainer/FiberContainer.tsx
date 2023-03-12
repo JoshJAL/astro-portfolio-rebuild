5e914d0211bab4a9ad1ea8f7191496ee8b65bd81
Joshua Levine <joshuaalevine3@gmail.com>
2023-03-12 03:22:16 -0400

    change cursor on 3d trees

--- src/components/fiberContainer/FiberContainer.tsx
+++ src/components/fiberContainer/FiberContainer.tsx
@@ -4,10 +4,14 @@ import { Scene } from '../scene/Scene';
 import { Ground } from '../ground/Ground';
 export function FiberContainer() {
   return (
-    <Canvas className='mt-4' camera={{ position: [14.4666, 2.0635, 5.556165], fov: 40 }} shadows>
+    <Canvas
+      className='mt-4 cursor-grab active:cursor-grabbing'
+      camera={{ position: [14.4666, 2.0635, 5.556165], fov: 40 }}
+      shadows
+    >
       <Scene />
       <Ground />
       <OrbitControls minDistance={1} maxDistance={200} />
     </Canvas>
-  )
+  );
 }
