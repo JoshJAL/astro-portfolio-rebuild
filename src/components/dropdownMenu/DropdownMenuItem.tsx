3ce46b518ad23ddf8d303508db003d75b22d2386
Joshua Levine <joshuaalevine3@gmail.com>
2023-04-26 23:54:12 -0400

    update consts, update links to be subdomains of main site

--- src/components/dropdownMenu/DropdownMenuItem.tsx
+++ src/components/dropdownMenu/DropdownMenuItem.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "@headlessui/react";
+import { Menu } from '@headlessui/react';
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
@@ -13,10 +13,13 @@ export default function DropdownMenuItem({ href, children }: DropdownMenuItemPro
   return (
     <Menu.Item>
       {(active) => (
-        <a href={href} className={classNames(active ? 'bg-orange-200 dark:bg-teal-700' : '', 'block px-4 py-2 text-sm')}>
+        <a
+          href={href}
+          className={classNames(active ? 'bg-orange-200 dark:bg-teal-700' : '', 'block px-4 py-2 text-sm')}
+        >
           {children}
         </a>
       )}
     </Menu.Item>
-  )
+  );
 }
--- src/consts.ts
+++ src/consts.ts
@@ -5,8 +5,10 @@ export const PROJECTS = '/projects';
 export const SOURCE = 'https://github.com/JoshJAL/astro-portfolio-rebuild';
 export const RESUME =
   'https://docs.google.com/document/d/1254COJOSGdsHpjNcSXf-VtdDhUxNAaB2nkCgap_aUQk/edit?usp=sharing';
-export const THINGS_I_USE = 'https://things-i-use.vercel.app/';
+export const THINGS_I_USE = 'https://tiu.joshualevine.me/';
 export const SITE_TITLE = 'Joshua Levine';
 export const SITE_DESCRIPTION =
   "My Portfolio! Please take some time to learn a little bit about me and don't hesitate to reach out!";
-export const THREE_D_PORTFOLIO = 'https://3d-portfolio-joshjal.vercel.app/';
+export const THREE_D_PORTFOLIO = 'https://3dportfolio.joshualevine.me/';
+
+export const OLD_PORTFOLIO = 'https://old.joshualevine.me/';
