696e531d43cd789fcee99bca81355a98e40656db
Joshua Levine <joshuaalevine3@gmail.com>
2023-06-01 14:45:26 -0400

    add pdf resume change link to resume

--- src/consts.ts
+++ src/consts.ts
@@ -3,8 +3,7 @@ export const EMAIL = 'JoshuaAlexanderLevine@gmail.com';
 export const PROJECTS = '/projects';
 // Might need to change this to a different link
 export const SOURCE = 'https://github.com/JoshJAL/astro-portfolio-rebuild';
-export const RESUME =
-  'https://docs.google.com/document/d/1254COJOSGdsHpjNcSXf-VtdDhUxNAaB2nkCgap_aUQk/edit?usp=sharing';
+export const RESUME = '/documents/J. Levine Resume 2023.pdf';
 export const THINGS_I_USE = 'https://tiu.joshualevine.me/';
 export const SITE_TITLE = 'Joshua Levine';
 export const SITE_DESCRIPTION =
