40b677066fe0651f01f811b86fa9f5abcc266bae
Joshua Levine <joshuaalevine3@gmail.com>
2023-06-15 20:33:32 -0400

    remove unnecessary logs

--- src/components/trees/Log.tsx
+++ src/components/trees/Log.tsx
@@ -6,25 +6,23 @@ import { GhibliShader } from '../../functions/GhibliShader';
 export const Log = forwardRef((props: any, ref) => {
   const { nodes } = useGLTF('/logs.glb') as any;
 
-  console.log(nodes)
-
   const uniforms = useMemo(() => {
     return {
       colorMap: {
-        value: props.colors,
+        value: props.colors
       },
       brightnessThresholds: {
         value: [0.9, 0.45, 0.001]
       },
       lightPosition: {
         value: new Vector3(15, 15, 15)
-      },
-    }
-  }, [props.colors])
+      }
+    };
+  }, [props.colors]);
 
   return (
     <group ref={ref} {...props} dispose={null}>
-      <mesh castShadow receiveShadow geometry={nodes.Log5.geometry} position={[0.33, -0.05, -0.68]} >
+      <mesh castShadow receiveShadow geometry={nodes.Log5.geometry} position={[0.33, -0.05, -0.68]}>
         <shaderMaterial attach={'material'} {...GhibliShader} uniforms={uniforms} />
       </mesh>
     </group>
